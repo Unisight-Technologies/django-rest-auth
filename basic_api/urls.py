1672a4f299c25b91afd96df16c694ff37ec0ba4f
Naitik0909 <parmarnaitik0909@gmail.com>
2021-08-16 23:47:13 +0530

    URLs modified

--- basic_api/urls.py
+++ basic_api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import RegisterUser, BlacklistToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(),name="register"),
-    path('logout/blacklist/', BlacklistToken.as_view(),name="blacklist")
+    path('logout/blacklist/', BlacklistToken.as_view(),name="blacklist"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
